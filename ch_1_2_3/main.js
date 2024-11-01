856ec5c4d15c89bbe662db47aed11c83e0aca40b
WANIYAM <164039644+WANIYAM@users.noreply.github.com>
2024-11-01 21:49:01 +0500

    Added assignment of ch_1_2_3

---
+++ ch_1_2_3/main.js
@@ -0,0 +1,4 @@
+var firstName = "Waniya";
+var lastName = "Mustafa";
+var age = 23;
+alert("hello! ".concat(firstName, " ").concat(lastName, " , you are ").concat(age, " years old "));
---
+++ ch_1_2_3/main.ts
@@ -0,0 +1,6 @@
+var firstName ="Waniya";
+var lastName ="Mustafa";
+var age = 23;
+
+alert(`hello! ${firstName} ${lastName} , you are ${age} years old `);
+
